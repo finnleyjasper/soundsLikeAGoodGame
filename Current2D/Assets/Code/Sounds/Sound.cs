f954b5cf4dd67e463616f8390534d307dbc51daf
CadLee <77828405+CadLee@users.noreply.github.com>
2025-04-04 22:07:47 +1100

    Sound update
    + Direction sound for Left/Right (80% both sides)
    + Sound plays quietly if player is on the Sound space (Very Bad Code 🤮)
    + Adjusted sound Colliders for the new size (same issue as movement)

--- Current2D/Assets/Code/Sounds/Sound.cs
+++ Current2D/Assets/Code/Sounds/Sound.cs
@@ -9,34 +9,61 @@ public class Sound : MonoBehaviour
 {
     public AudioSource audioSource;
     private LayerMask playerLayer;
+    private LayerMask playerSoundLayer;
 
     [SerializeField] private float loopDelay; // how many seconds between each repeat of the sound
     private float timeLastPlayed = 0;
 
     public float defaultVolume = 1; // can change this in the editor if we want some sounds quieter than others
 
+    private bool leaveFlag = false; // to check whether the player has left the sound space
 
     void Start()
     {
         audioSource = gameObject.GetComponent<AudioSource>();
         audioSource.volume = 0;
 
-        playerLayer = LayerMask.GetMask("PlayerSoundDetection");
-
+        playerLayer = LayerMask.GetMask("Player");
+        playerSoundLayer = LayerMask.GetMask("PlayerSoundDetection");
     }
 
     void Update()
     {
-        // checks to see if any of the player's sound detection colliders are on top of this object
+        // checks to see if the player's colliders are on top of this object
         Collider2D player = Physics2D.OverlapPoint(new UnityEngine.Vector2(gameObject.transform.position.x, gameObject.transform.position.y), playerLayer);
 
+        // checks to see if any of the player's sound detection colliders are on top of this object
+        Collider2D soundcol = Physics2D.OverlapPoint(new UnityEngine.Vector2(gameObject.transform.position.x, gameObject.transform.position.y), playerSoundLayer);
+
         // if the player is near enough to activate, AND enough time has passed between loops, AND the sound isnt already playing
-        if (Time.time >= timeLastPlayed + loopDelay && player && !audioSource.isPlaying)
+        if (Time.time >= timeLastPlayed + loopDelay && soundcol && !audioSource.isPlaying)
         {
             timeLastPlayed = Time.time;
             audioSource.Play();
-
             Debug.Log(gameObject.name + " has played a sound");
+        }
+
+        // if player is on the sound space
+        if (player)
+        {
+            audioSource.volume = defaultVolume/1.7f;
+            if (Time.time >= timeLastPlayed + loopDelay && !audioSource.isPlaying)
+            {
+                timeLastPlayed = Time.time;
+                audioSource.Play();
+
+                Debug.Log(gameObject.name + " has played a sound");
+            }
+            leaveFlag = true;
+            Debug.Log("Player is on \""+gameObject.name+"\" Space");
+        }
+        else if (!player)
+        {
+            if (leaveFlag == true)
+            {
+                leaveFlag = false;
+                audioSource.volume = 0;
+            }
         }
     }
 }
